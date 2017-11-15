cf927c3ee5963bc71ead6c5e04f161c6b545471f
Sérgio Santos <me@s3rgiosan.com>
2017-11-15 16:20:38 +0000

    Fixed loading of mu-plugins residing in subdirectories

--- wp-content/mu-plugins/loader.php
+++ wp-content/mu-plugins/loader.php
@@ -9,17 +9,61 @@
  * License:     GPL-2.0+
  */
 
+namespace WPSeed\WP\Plugin\Loader;
+
 if ( ( defined( 'WP_INSTALLING' ) && WP_INSTALLING ) ) {
 	return;
 }
 
+require_once ABSPATH . 'wp-admin/includes/plugin.php';
+
+/**
+ * Get list of must-use plugins residing in subdirectories.
+ *
+ * @since  1.0.0
+ * @param  string $cache_key Cache key to store plugins under.
+ * @return array             List of plugin files to load.
+ */
+function get_mu_plugins( $cache_key = 'mu_plugins_cache' ) {
+
+	$mu_plugins = \get_site_transient( $cache_key );
+	if ( is_array( $mu_plugins ) ) {
+		foreach ( $mu_plugins as $plugin_file => $plugin_data ) {
+			if ( ! is_readable( WPMU_PLUGIN_DIR . '/' . $plugin_file ) ) {
+				$mu_plugins = array();
+				break;
+			}
+		}
+
+		if ( ! empty( $mu_plugins ) ) {
+			return $mu_plugins;
+		}
+	}
+
+	$plugins = \get_plugins( '/../mu-plugins/' );
+
+	$mu_plugins = array();
+	foreach ( array_keys( $plugins ) as $plugin_file ) {
+		if ( dirname( $plugin_file ) !== '.' ) {
+			$mu_plugins[ $plugin_file ] = \get_plugin_data( WPMU_PLUGIN_DIR . '/' . $plugin_file );
+		}
+	}
+
+	\set_site_transient( $cache_key, $mu_plugins );
+
+	return $mu_plugins;
+}
+
+/**
+ * Show our own must-use plugins in the Network Admin.
+ *
+ * @since 1.0.0
+ */
 \add_action( 'pre_current_active_plugins', function () {
 
 	global $plugins, $wp_list_table;
 
-	$mu_plugins = \get_mu_plugins();
-
-	// Add our own mu-plugins to the page.
+	$mu_plugins = get_mu_plugins();
 	foreach ( $mu_plugins as $plugin_file => $plugin_data ) {
 
 		if ( empty( $plugin_data['Name'] ) ) {
@@ -58,17 +102,27 @@ if ( ( defined( 'WP_INSTALLING' ) && WP_INSTALLING ) ) {
 		'total_items' => $total_this_page,
 		'per_page'    => $plugins_per_page,
 	) );
-});
+} );
 
-\add_filter( 'network_admin_plugin_action_links', function ( $actions, $plugin_file, $plugin_data, $context ) {
+/**
+ * Load must-use plugins.
+ *
+ * @since 1.0.0
+ */
+\add_action( 'muplugins_loaded', function () {
 
-	$mu_plugins = \get_mu_plugins();
+	$cache_key   = 'mu_plugins_cache';
+	$flush_cache = false;
 
-	if ( $context !== 'mustuse' || ! array_key_exists( $plugin_file, $mu_plugins, true ) ) {
-		return $actions;
+	if ( isset( $_SERVER['REQUEST_URI'] ) ) {
+		$flush_cache = strpos( $_SERVER['REQUEST_URI'], '/wp-admin/plugins.php' ) !== false;
 	}
 
-	$actions[] = sprintf( '<span style="color:#333">File: <code>%s</code></span>', $plugin_file );
+	if ( $flush_cache ) {
+		\delete_site_transient( $cache_key );
+	}
 
-	return $actions;
-}, 10, 4 );
+	foreach ( get_mu_plugins( $cache_key ) as $plugin_file => $plugin_data ) {
+		include_once WPMU_PLUGIN_DIR . '/' . $plugin_file;
+	}
+} );
